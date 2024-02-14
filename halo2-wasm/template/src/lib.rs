d27895342ae37da6ecba899b6d5002ccd6493c66
Jędrzej Kula <jedrzej.kula@cardinals.cc>
2024-02-14 11:35:24 +0100

    update dependencies

--- halo2-wasm/template/src/lib.rs
+++ halo2-wasm/template/src/lib.rs
@@ -1,8 +1,8 @@
-use wasm_bindgen::prelude::*;
-use std::{cell::RefCell, sync::Arc};
 use halo2_base::gates::circuit::builder::BaseCircuitBuilder;
 use halo2_base::gates::flex_gate::{GateChip, GateInstructions};
 use halo2_base::halo2_proofs::halo2curves::bn256::Fr;
+use std::{cell::RefCell, rc::Rc};
+use wasm_bindgen::prelude::*;
 
 use halo2_wasm::Halo2Wasm;
 
@@ -10,7 +10,7 @@ use halo2_wasm::Halo2Wasm;
 pub struct MyCircuit {
     // Add whatever other chips you need here
     gate: GateChip<Fr>,
-    builder: Arc<RefCell<BaseCircuitBuilder<Fr>>>,
+    builder: Rc<RefCell<BaseCircuitBuilder<Fr>>>,
 }
 
 #[wasm_bindgen]
@@ -18,17 +18,24 @@ impl MyCircuit {
     #[wasm_bindgen(constructor)]
     pub fn new(circuit: &Halo2Wasm) -> Self {
         let gate = GateChip::new();
-        Halo2WasmTemplate {
+        MyCircuit {
             gate,
-            builder: Arc::clone(&circuit.circuit),
+            builder: Rc::clone(&circuit.circuit),
         }
     }
 
     pub fn run(&mut self) {
         // Replace with your circuit, making sure to use `self.builder`
-        let a = self.builder.borrow_mut().main(0).load_witness(Fr::from(1u64));
-        let b = self.builder.borrow_mut().main(0).load_witness(Fr::from(2u64));
+        let a = self
+            .builder
+            .borrow_mut()
+            .main(0)
+            .load_witness(Fr::from(1u64));
+        let b = self
+            .builder
+            .borrow_mut()
+            .main(0)
+            .load_witness(Fr::from(2u64));
         self.gate.add(self.builder.borrow_mut().main(0), a, b);
     }
-
 }
